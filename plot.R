87910780826e36425c5d00948d2e76406898d892
Ian Hardesty <Ian.W.Hardesty-1@ou.edu>
2025-02-07 10:35:45 -0600

    simple function test

---
+++ plot.R
@@ -0,0 +1,12 @@
+#' Plot
+#'
+#' @param x x as independent variable
+#'
+#' @returns A plot
+#' @export
+#'
+#' @examples
+#' \dontrunmyplot
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
