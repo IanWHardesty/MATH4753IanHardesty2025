cad0abc3b474cb9e85b351c2ef447499c1f163de
IanWHardesty <Ian.W.Hardesty-1@ou.edu>
2025-05-01 10:46:06 -0500

    Add files via upload
    Simple plot

---
+++ R/myplot.R
@@ -0,0 +1,12 @@
+#' Plot
+#'
+#' @param x x as independent variable
+#'
+#' @returns A plot
+#' @export
+#'
+#'@examples
+#'myplot(3)
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
